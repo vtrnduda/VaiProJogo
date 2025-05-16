b75e3a45d79e7eddb785dd348bf3748dd27ed992
vtrnduda <vtrnduda@gmail.com>
2025-05-16 18:33:09 -0300

    feat: implementa conexao com db4o ativando cascade

--- src/appconsole/Util.java
+++ src/appconsole/Util.java
@@ -1,6 +1,8 @@
 package appconsole;
 
 import model.Categoria;
+import model.Ingresso;
+import model.Jogo;
 
 public class Util {
 
@@ -12,6 +14,19 @@ public class Util {
 
 		EmbeddedConfiguration config =  Db4oEmbedded.newConfiguration(); 
 		config.common().messageLevel(0); 
+
+		config.common().objectClass(Categoria.class).cascadeOnDelete(true);;
+		config.common().objectClass(Categoria.class).cascadeOnUpdate(true);;
+		config.common().objectClass(Categoria.class).cascadeOnActivate(true);
+		config.common().objectClass(Ingresso.class).cascadeOnDelete(true);;
+		config.common().objectClass(Ingresso.class).cascadeOnUpdate(true);;
+		config.common().objectClass(Ingresso.class).cascadeOnActivate(true);
+		config.common().objectClass(Jogo.class).cascadeOnDelete(true);;
+		config.common().objectClass(Jogo.class).cascadeOnUpdate(true);;
+		config.common().objectClass(Jogo.class).cascadeOnActivate(true);
+
+		manager = Db4oEmbedded.openFile(config, "banco.db4o");
+		return manager;
 	}
 
 	public static void desconectar() {
