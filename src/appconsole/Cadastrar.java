4b5462ccd734a621f207fc63fb1968d6c7940972
maduvitorino <maria.vitorino@gran.com>
2025-05-20 00:29:47 -0300

    feat: implementa classes console

--- src/appconsole/Cadastrar.java
+++ src/appconsole/Cadastrar.java
@@ -6,6 +6,10 @@ import model.Categoria;
 import model.Ingresso;
 import model.Jogo;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
 public class Cadastrar {
 
     private ObjectContainer manager;
@@ -20,6 +24,56 @@ public class Cadastrar {
         Categoria cat2 = new Categoria(2, 250.0);
         Categoria cat3 = new Categoria(3, 350.0);
 
+        manager.store(cat1);
+        manager.store(cat2);
+        manager.store(cat3);
+        manager.commit();
+
+        // Criar jogos
+        Jogo jogo1 = new Jogo(
+                "15/10/2025 16:00",
+                "Estádio Maracanã",
+                "Flamengo",
+                "Fluminense"
+        );
+
+        Jogo jogo2 = new Jogo(
+                "22/10/2025 16:00",
+                "Arena Itaquera",
+                "Corinthians",
+                "Palmeiras"
+        );
+
+        Jogo jogo3 = new Jogo(
+                "05/11/2025 18:30",
+                "Mineirão",
+                "Cruzeiro",
+                "Atlético-MG"
+        );
+
+        manager.store(jogo1);
+        manager.store(jogo2);
+        manager.store(jogo3);
+        manager.commit();
+
+        // Criar ingressos
+        Ingresso ingresso1 = new Ingresso(jogo1, cat1);
+        Ingresso ingresso2 = new Ingresso(jogo1, cat2);
+        Ingresso ingresso3 = new Ingresso(jogo1, cat3);
+
+        Ingresso ingresso4 = new Ingresso(jogo2, cat1);
+        Ingresso ingresso5 = new Ingresso(jogo2, cat2);
+
+        Ingresso ingresso6 = new Ingresso(jogo3, cat1);
+        Ingresso ingresso7 = new Ingresso(jogo3, cat3);
+
+        manager.store(ingresso1);
+        manager.store(ingresso2);
+        manager.store(ingresso3);
+        manager.store(ingresso4);
+        manager.store(ingresso5);
+        manager.store(ingresso6);
+        manager.store(ingresso7);
         manager.commit();
 
         System.out.println("Cadastro concluído com sucesso!");
--- src/appconsole/Listar.java
+++ src/appconsole/Listar.java
@@ -1,4 +1,48 @@
 package appconsole;
 
+
+import com.db4o.ObjectContainer;
+import com.db4o.ObjectSet;
+import com.db4o.query.Query;
+
+import model.Categoria;
+import model.Ingresso;
+import model.Jogo;
+
+import java.util.List;
+
 public class Listar {
+    private static ObjectContainer manager;
+    public static void main(String[] args) {
+        manager = Util.conectar();
+
+        Query qc = manager.query();
+        System.out.println("\n---listagem das Categorias:");
+
+        qc.constrain(Categoria.class);
+        List<Categoria> categorias = qc.execute();
+        for (Categoria c : categorias) {
+            System.out.println(c);
+        }
+
+        Query qj = manager.query();
+        System.out.println("\n---listagem dos Jogos:");
+
+        qj.constrain(Jogo.class);
+        List<Jogo> jogos = qj.execute();
+        for (Jogo j : jogos) {
+            System.out.println(j);
+        }
+
+        Query qi = manager.query();
+        System.out.println("\n---listagem dos Ingressos:");
+
+        qi.constrain(Ingresso.class);
+        List<Ingresso> ingressos = qi.execute();
+        for (Ingresso i : ingressos) {
+            System.out.println(i);
+        }
+
+        Util.desconectar();
+        }
 }
