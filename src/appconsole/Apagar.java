4158a7b2a2211094c10b8b465790a31cc9ce267e
maduvitorino <maria.vitorino@gran.com>
2025-05-23 15:23:23 -0300

    feat: modifica alterar e apagar para considerar relacionamentos

--- src/appconsole/Apagar.java
+++ src/appconsole/Apagar.java
@@ -6,25 +6,56 @@ import com.db4o.ObjectContainer;
 import com.db4o.query.Query;
 
 import model.Ingresso;
+import model.Jogo;
 
 public class Apagar {
     private static ObjectContainer manager;
 
     public static void main(String[] args) {
         manager = Util.conectar();
-        System.out.println("Apagar o Ingresso CRUATL648529");
+        System.out.println("Apagar o Ingresso");
 
-        // localizar inrgesso com numero CRUATL648529
+        // localizar inrgesso a partir do codigo dele
         Query q = manager.query();
         q.constrain(Ingresso.class);
-        q.descend("codigo").constrain("CRUATL648529");
+        q.descend("codigo").constrain("FLAFLU750932"); //Inserir Codigo do ingresso
         List<Ingresso> resultado = q.execute();
+        
+        if (resultado.isEmpty()) {
+            System.out.println("Ingresso não encontrado!");
+            Util.desconectar();
+            return;
+        }
+        
+        Ingresso ingresso = resultado.getFirst();
 
-        if (!resultado.isEmpty()) {
-            Ingresso ingresso = resultado.getFirst();
+        Query qj = manager.query();
+        qj.constrain(Jogo.class);
+        qj.descend("id").constrain(ingresso.getJogo().getId());
+        List<Jogo> jogo = qj.execute();
+        
+        if (jogo.isEmpty()) {
+            System.out.println("Jogo não encontrado!");
+            Util.desconectar();
+            return;
+        }
+        
+        Jogo jogoResultado = jogo.getFirst();
 
-            manager.delete(ingresso);
-            manager.commit();
+        List<Ingresso> listaIngresso = jogoResultado.getListaIngressos();
+        for (Ingresso i : listaIngresso) {
+            if (i.getCodigo().equals(ingresso.getCodigo())) {
+                
+                jogoResultado.getListaIngressos().remove(i); // remove o ingresso da lista do jogo
+                
+                manager.delete(i); // deleta o ingresso do banco
+                
+                manager.store(jogoResultado); //atualiza o jogo no banco
+                manager.commit();
+                
+                System.out.println("Ingresso removido com sucesso!");
+                break;
+            }
         }
 
         Util.desconectar();
