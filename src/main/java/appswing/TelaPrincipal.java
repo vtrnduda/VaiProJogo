7edf8c0c8c1f1f961b03ce73d85355054ad25d09
Julielison <julielisonlima11@gmail.com>
2025-08-27 01:00:00 -0300

    feat: tela principal com logo

--- src/main/java/appswing/TelaPrincipal.java
+++ src/main/java/appswing/TelaPrincipal.java
@@ -13,8 +13,98 @@ import javax.swing.JMenu;
 import javax.swing.JMenuBar;
 import javax.swing.SwingConstants;
 
-import requisito.Fachada;
-
 public class TelaPrincipal {
+    private JFrame frame;
+    private JMenu mnJogo;
+    private JMenu mnIngresso;
+    private JMenu mnCategoria;
+    private JMenu mnConsulta;
+    private JLabel label;
 
+
+
+    /**
+     * Launch the application.
+     */
+    public static void main(String[] args) {
+        EventQueue.invokeLater(new Runnable() {
+            public void run() {
+                try {
+                    TelaPrincipal window = new TelaPrincipal();
+                    window.frame.setVisible(true);
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
+        });
+    }
+
+    /**
+     * Create the application.
+     */
+    public TelaPrincipal() {
+        initialize();
+        frame.setVisible(true);
+    }
+
+    /**
+     * Initialize the contents of the frame.
+     */
+    private void initialize() {
+        frame = new JFrame();
+        frame.setTitle("Vai pro Jogo");
+        frame.setBounds(100, 100, 411, 440);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.getContentPane().setLayout(null);
+
+        label = new JLabel("");
+        label.setFont(new Font("Tahoma", Font.PLAIN, 26));
+        label.setHorizontalAlignment(SwingConstants.CENTER);
+        label.setText("Inicializando...");
+        label.setBounds(0, 0, 423, 382);
+        //label.setBounds(0, 0, frame.getWidth(), frame.getHeight());
+        ImageIcon imagem = new ImageIcon(getClass().getResource("/vaiProJogo-logo.png"));
+        imagem = new ImageIcon(imagem.getImage().getScaledInstance(label.getWidth(),label.getHeight(), Image.SCALE_DEFAULT));//		label.setIcon(fotos);
+        label.setIcon(imagem);
+        frame.getContentPane().add(label);
+        frame.setResizable(false);
+
+        JMenuBar menuBar = new JMenuBar();
+        frame.setJMenuBar(menuBar);
+        mnJogo = new JMenu("Jogo");
+        mnJogo.addMouseListener(new MouseAdapter() {
+            @Override
+            public void mouseClicked(MouseEvent e) {
+                new TelaJogo();
+            }
+        });
+        menuBar.add(mnJogo);
+
+        mnIngresso = new JMenu("Ingresso");
+        mnIngresso.addMouseListener(new MouseAdapter() {
+            @Override
+            public void mouseClicked(MouseEvent e) {
+                new TelaIngresso();
+            }
+        });
+        menuBar.add(mnIngresso);
+
+        mnCategoria = new JMenu("Categoria");
+        mnCategoria.addMouseListener(new MouseAdapter() {
+            @Override
+            public void mouseClicked(MouseEvent e) {
+                new TelaCategoria();
+            }
+        });
+        menuBar.add(mnCategoria);
+
+        mnConsulta = new JMenu("Consultas");
+        mnConsulta.addMouseListener(new MouseAdapter() {
+            @Override
+            public void mouseClicked(MouseEvent e) {
+                new TelaConsulta();
+            }
+        });
+        menuBar.add(mnConsulta);
+    }
 }
